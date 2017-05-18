c31536759d00c531ff4b5b383d755a48a06f467f
Porscha07 <porscha.uga@gmail.com>
2017-05-18 10:46:39 -0400

    card-flip, need to edit further

---
+++ css-201/card-flip.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	// console.log("sanity check");
+	// $('#parent').click(function(){
+	// 	$(this).css({
+	// 		'perspective':500px
+	// 	})
+		$('change-perspective').change(function(event){
+			// console.log("event")
+			// console.log($(this).val())
+			var newVal = $(this).val();
+				$('#parent').css({
+					'perspective': newVal
+				})
+		});
+
+
+		$('change-loc').change(function(event){
+			// console.log("event")
+			// console.log($(this).val())
+			var newVal = $(this).val();
+				$('#parent').css({
+					'perspective-origin': newVal
+				})
+		});
+		
+		$('card-holder').click(function(){
+			$(this).toggleClass('flip')
+		});
+})
